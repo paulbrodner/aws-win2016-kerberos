994bac081e406fbbb347f57499a1f139f0aa6b0a
Paul Brodner <paul.brodner@gmail.com>
2019-10-10 11:47:18 +0300

    adding videos

--- kerberos-environment/files/setup-server.ps1
+++ kerberos-environment/files/setup-server.ps1
@@ -9,12 +9,13 @@ $subnet=((ipconfig | findstr [0-9].\.)[1]).Split()[-1]
 $gw=((ipconfig | findstr [0-9].\.)[2]).Split()[-1]
 $dns=(netsh interface ip show dnsservers | findstr [0-9].\.).Split()[-1]
 
+Get-ADDomain -Current LocalComputer
+
 # this user is already created and exist here
 # we only need to add it to Domain Admins so we can remotely loggin with it
 net group "Domain Admins" ${SERVER_ADMIN_USERNAME}  /add
 
 Write-Host "Sucessfully added ${SERVER_ADMIN_USERNAME} to Domain Admins"
-start-sleep -Seconds 20
 
 $domain = "${DOMAIN}.${HOSTED_ZONE}"
 $domain_array = $domain.split('.')
@@ -26,6 +27,25 @@ foreach ($domcomp in $domain_array) {
 }
 
 Write-Host "Preparing $domain_path users..."
+
+try {
+   Get-ADDomain -Current LocalComputer
+   Write-Output "AD running"
+ } 
+ catch {
+   Write-Output "AD not running, sleeping for 60 seconds.."
+   start-sleep -Seconds 60
+ }
+
+# somethimes this starts slow
+ try {
+   Get-ADDomain -Current LocalComputer
+   Write-Output "AD running"
+ } 
+ catch {
+   Write-Output "AD not running, sleeping for 60 seconds.."
+   start-sleep -Seconds 60
+ }
 
 #Create kerberos authentication user used for creating keytabs
 Write-Host "Creating KERBEROS_ADMIN_USERNAME: [${KERBEROS_ADMIN_USERNAME}] in domain: [$domain_path]"
@@ -46,11 +66,11 @@ Write-Host  "Successfully added KERBEROS_TEST_USERNAME : [${KERBEROS_TEST_USERNA
 
 # # generate kerberos keys
 # # https://docs.alfresco.com/5.1/tasks/auth-kerberos-cross-domain.html
+$REALM="${DOMAIN}.${HOSTED_ZONE}".ToUpper()
+$NETBIOSNAME = "${DOMAIN}".ToUpper()
 
-# $realm="${DOMAIN}.${HOSTED_ZONE}".ToUpper()
-# $netbiosname = "${DOMAIN}".ToUpper()
-# ktpass -princ HTTP/${FQDN}@<$realm> -pass $KERBPASS -mapuser $netbiosname\${KERBEROS_ADMIN_USERNAME} -crypto all -ptype KRB5_NT_PRINCIPAL -out c:\httpkerberos.keytab -kvno 0
-# setspn -a HTTP/${FQDN} ${KERBEROS_ADMIN_USERNAME}
+ktpass -princ HTTP/$FQDN@<$REALM> -pass ${KERBEROS_ADMIN_PASSWORD} -mapuser $NETBIOSNAME\${KERBEROS_ADMIN_USERNAME} -crypto all -ptype KRB5_NT_PRINCIPAL -out c:\httpkerberos.keytab -kvno 0
+setspn -a HTTP/$FQDN ${KERBEROS_ADMIN_USERNAME}
 
 netsh interface ip set dns "Ethernet" static "127.0.0.1"
 netsh interface ip add dns name="Ethernet" addr=$dns index=2
